b3404a47e44569eb90cb89c4e12ebbe443db2f4e
Arseny Korotaev <s3v3nny@ya.ru>
2023-11-21 00:02:52 +0400

    sdadasd

--- src/main/java/ru/s3v3nny/smartdevices/Main.java
+++ src/main/java/ru/s3v3nny/smartdevices/Main.java
@@ -2,7 +2,7 @@ package ru.s3v3nny.smartdevices;
 
 public class Main {
     public static void main(String[] args) {
-        int port = 8080;
+        int port = 9555;
         SmartDevicesServer server = new SmartDevicesServer();
         server.start(port);
     }
